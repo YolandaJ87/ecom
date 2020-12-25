419287104db0c1cdf612d790fd963a8e86602156
YolandaJ87 <yolanda_j87@hotmail.com>
2020-12-25 13:51:50 +0100

    setup reduxform

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component{
+    render (){
+        return (
+            <div className="sign-in">
+                sign innnn
+            </div>
+        );
+    };
+}
+
+export default SignIn;
