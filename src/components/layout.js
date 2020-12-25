b4d07454800505ebe7862c29e30aeecf123f658e
YolandaJ87 <yolanda_j87@hotmail.com>
2020-12-25 12:23:26 +0100

    construccion cuadricula, cabecera y componente de navegacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './headernavbar/header';
+import Navbar from './headernavbar/navbar';
 
 class Layout extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>ecomr</h1>
-        {/* {...this.props.children} */}
+      <div className='layout'>
+        {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
