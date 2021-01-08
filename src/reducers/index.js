691a45c0bb6c222fd979401d4528bbb0c4aa4af8
YolandaJ87 <yolanda_j87@hotmail.com>
2021-01-08 19:27:02 +0100

    construir headernavbar reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 //nuevo
 import { reducer as form } from 'redux-form';
+import HeaderNavbar from './headernavbarReducer';
 
 const rootReducer = combineReducers({
-  form
+  form,
   // state: (state = {}) => state
+  HeaderNavbar
 });
 
 export default rootReducer;
