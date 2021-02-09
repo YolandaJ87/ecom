30c87905f7fd78483dfb84a428d13eb25229e457
YolandaJ87 <yolanda_j87@hotmail.com>
2021-02-09 20:02:23 +0100

    construción base shop component y ruta

---
+++ src/components/shop/shop.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component{
+    componentDidMount(){
+        //set header links
+        const headerLinks = [
+            { 
+                _id: 0, 
+                title: 'Login',
+                path: '/signin'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks);
+        //fetch shop products action creator
+        //fetch navbar links
+            //set navbar link --> donde nos permitira filtrar por tipo de Productos a mostrar en el componente
+                //all   javascrip ui/ux linux   python  uml ruby
+        
+    }
+    render (){
+        return( 
+            <div className="shop">  
+            {/* barra de busqueda */}
+            {/* componente de productos */}
+            {/* boton carro compra */}
+                Shop ....
+            </div>
+        )
+    };
+}
+function mapStateToProps( state ){
+    return{state};
+}
+Shop=connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
