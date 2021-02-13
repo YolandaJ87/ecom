b60fe2b43c1893c65cd88d8a4812dfeece078bd6
YolandaJ87 <yolanda_j87@hotmail.com>
2021-02-13 18:29:02 +0100

    solucionamos error npm del 11022021

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -11,12 +11,10 @@ const INITIAL_STATE = {
 
 export default function(state = INITIAL_STATE, action){
     switch (action.type){
-        case SET_SHOP_CATEGORIES:
+        // case SET_SHOP_CATEGORIES:
             // return{
             //     ...state,
-            //     categories: action.payload
             // }
-        default:
-            break;
+        default: return state;
     }
 }
