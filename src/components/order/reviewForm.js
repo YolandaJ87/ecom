a7c6cbc791cc09fdafe62b5bf2c13547dd8a7e82
YolandaJ87 <yolanda_j87@hotmail.com>
2021-03-06 22:47:43 +0100

    barra titulo detale compra

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -13,23 +13,28 @@ class ReviewForm extends Component{
         
         return (
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <div className='review-form__legend'>
+                    <div className='review-form__legend__name'>Name</div>
+                    <div className='review-form__legend__quantity'>Quantity</div>
+                    <div className='review-form__legend__price'>Price</div>
+                </div>
                 <ReviewProducts className='review-form__products' />
                 
-                <div className='review-form__line'></div>
+                <div className='review-form__line'/>
                 <Field className='review-form__proceed'
-                onClick={() => history.push('/signin')}
-                type='submit'
-                title='Proceed to Checkout'
-                name='proceed'
-                component={FormButton}/>
+                    onClick={() => history.push('/signin')}
+                    type='submit'
+                    title='Proceed to Checkout'
+                    name='proceed'
+                    component={FormButton}/>
                 
                 <Field className='review-form__back'
-                onClick={() => history.push('/shop')}
-                type='button'
-                title='Back'
-                name='back'
-                short={true}
-                component={FormButton}/>
+                    onClick={() => history.push('/shop')}
+                    type='button'
+                    title='Back'
+                    name='back'
+                    short={true}
+                    component={FormButton}/>
             </form>
         );
     };
